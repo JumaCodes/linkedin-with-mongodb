c3a68dfef4bf2db476a37ff77bf7c90cd8f356dc
CodexLegacy <73387073+CodexLegacy@users.noreply.github.com>
2022-05-09 00:48:52 +0100

    Full Stack linkedin with nextAuth, mongoDb, tailwindcss and realtime database. #fullstack

---
+++ next.config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  images: {
+    domains: ["rb.gy"],
+    dangerouslyAllowSVG: true,
+  },
+}
+
---
+++ pages/_app.js
@@ -0,0 +1,18 @@
+import { ThemeProvider } from 'next-themes'
+import '../styles/globals.css'
+import { SessionProvider } from "next-auth/react";
+import { RecoilRoot } from "recoil";
+
+function MyApp({ Component, pageProps: {session, ...pageProps} }) {
+  return (
+    <SessionProvider session={session}>
+      <RecoilRoot>
+        <ThemeProvider attribute="class">
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </RecoilRoot>
+    </SessionProvider>
+  )
+}
+
+export default MyApp
